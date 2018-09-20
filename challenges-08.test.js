90ce22193caef1acc36466b73825c61f37e1ac0e
GREATBIGNATE <greatbignate@gmail.com>
2018-09-19 20:26:38 -0700

    all done, one line per challenge

--- challenges-08.test.js
+++ challenges-08.test.js
@@ -164,6 +164,7 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 
 const evenOddNumericValues = (input) => {
   // Solution code here...
+  return input.filter(input => typeof input === 'number').map(number => number%2 ? 'odd' : 'even');
 };
 
 /* ------------------------------------------------------------------------------------------------
